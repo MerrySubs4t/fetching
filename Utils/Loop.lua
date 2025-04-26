f4d09b8ba1347d64d4f2f4976cb59ae57529146e
9sixt1nwza <merrysubs4t@gmail.com>
2025-04-27 02:24:21 +0700

    Update Loop.lua

--- Utils/Loop.lua
+++ Utils/Loop.lua
@@ -1,23 +1,7 @@
-local LoopEnv_ = {}
-LoopEnv_.__index = LoopEnv_
-
-function LoopEnv_.new(body)
-	local self = setmetatable({}, LoopEnv_)
-	self.body = body
+return function(body)
 	task.spawn(function()
 		while task.wait() do
-			self.body()
+			body()
 		end
 	end)
-	return self
 end
-
-function LoopEnv_:callback(new)
-	if typeof(new) == 'function' then
-		self.body = new
-	else
-		return
-	end
-end
-
-return LoopEnv_
