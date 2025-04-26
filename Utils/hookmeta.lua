988015e11fad71995e0d881471db4660deabdcb7
9sixt1nwza <merrysubs4t@gmail.com>
2025-04-27 02:07:10 +0700

    Update hookmeta.lua

--- Utils/hookmeta.lua
+++ Utils/hookmeta.lua
@@ -6,32 +6,32 @@ local islclosure = (not Blacklist_Executor and islclosure) or (function(...) ret
 local sethiddenproperty = sethiddenproperty or (function(...) return ... end)
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 task.delay(2, function()
-	pcall(function()
-		if Blacklist_Executor then
-			--if not _G.UnlockXeno then
-			--	LocalPlayer:Kick("Current Executor is Not Support: " .. tostring(identifyexecutor()))
-			--end
-		else
-			if hookfunction and not islclosure(hookfunction) then
+	if Blacklist_Executor then
+		--if not _G.UnlockXeno then
+		--	LocalPlayer:Kick("Current Executor is Not Support: " .. tostring(identifyexecutor()))
+		--end
+	else
+		if hookfunction and not islclosure(hookfunction) then
+			pcall(function()
 				hookfunction(require(ReplicatedStorage:WaitForChild("Effect").Container.Death), function()
-					return
+					
 				end)
 				hookfunction(require(ReplicatedStorage:WaitForChild("Effect").Container.Respawn), function()
-					return
+					
 				end)
 				hookfunction(require(ReplicatedStorage:WaitForChild("Effect").Container.RaceAwakenings.CyborgDrone), function()
-					return
+					
 				end)
 				hookfunction(require(ReplicatedStorage:WaitForChild("GuideModule")).ChangeDisplayedNPC, function()
 					return
 				end)
-			end
-			if require then
-				pcall(function()
-					local Camera = require(ReplicatedStorage:WaitForChild("Util").CameraShaker)
-					Camera:Stop()
-				end)
-			end
+			end)
 		end
-	end)
+		if require then
+			pcall(function()
+				local Camera = require(ReplicatedStorage:WaitForChild("Util").CameraShaker)
+				Camera:Stop()
+			end)
+		end
+	end
 end)
