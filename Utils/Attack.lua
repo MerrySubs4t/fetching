785d50d395e20c91a2d49ac9537b232a010a2eb2
9sixt1nwza <merrysubs4t@gmail.com>
2025-04-27 15:22:28 +0700

    Update Attack.lua

--- Utils/Attack.lua
+++ Utils/Attack.lua
@@ -6,51 +6,50 @@ local Net = ReplicatedStorage:WaitForChild("Modules"):WaitForChild("Net")
 local RegisterHit = Net:WaitForChild("RE/RegisterHit")
 local RegisterAttack = Net:WaitForChild("RE/RegisterAttack")
 
+local PlayerScripts = LocalPlayer:WaitForChild("PlayerScripts")
+local LocalScript = PlayerScripts:FindFirstChildOfClass("LocalScript")
+while not LocalScript do
+	LocalPlayer.PlayerScripts.ChildAdded:Wait()
+	LocalScript = PlayerScripts:FindFirstChildOfClass("LocalScript")
+end
+
 local HIT_FUNCTION
-
-task.defer(function()
-	local PlayerScripts = LocalPlayer:WaitForChild("PlayerScripts")
-	local LocalScript = PlayerScripts:FindFirstChildOfClass("LocalScript")
-	while not LocalScript do
-		LocalPlayer.PlayerScripts.ChildAdded:Wait()
-		LocalScript = PlayerScripts:FindFirstChildOfClass("LocalScript")
-	end
-
-	if getsenv then
-		pcall(function()
-			local Success, ScriptEnv = pcall(getsenv, LocalScript)
-
-			if Success and ScriptEnv then
-				HIT_FUNCTION = ScriptEnv._G.SendHitsToServer
-			end
-		end)
-	end
-end)
+if getsenv then
+	pcall(function()
+		local Success, ScriptEnv = pcall(getsenv, LocalScript)
+		if Success and ScriptEnv then
+			HIT_FUNCTION = ScriptEnv._G.SendHitsToServer
+		end
+	end)
+end
 
 return function()
 	pcall(function()
-		for _, enemy in ipairs(workspace.Enemies:GetChildren()) do
+		local Character = LocalPlayer.Character
+		if not Character or not Character:FindFirstChild("HumanoidRootPart") then return end
+
+		local Enemies = workspace.Enemies:GetChildren()
+		for _, enemy in ipairs(Enemies) do
 			local hum = enemy:FindFirstChild("Humanoid")
 			local root = enemy:FindFirstChild("HumanoidRootPart")
-			if hum and hum.Health > 0 and root and (root.Position - LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 60 then
-				local args = {
-					[1] = root,
-					[2] = {}
-				}
-				for _, e in ipairs(workspace.Enemies:GetChildren()) do
-					if e:FindFirstChild("Humanoid") and e.Humanoid.Health > 0 then
-						table.insert(args[2], {
-							[1] = e,
-							[2] = e:FindFirstChild("HumanoidRootPart") or e:FindFirstChildOfClass("BasePart")
-						})
+			if hum and root and hum.Health > 0 and (root.Position - Character.HumanoidRootPart.Position).Magnitude <= 60 then
+				local targets = {}
+				for _, e in ipairs(Enemies) do
+					local eh = e:FindFirstChild("Humanoid")
+					local eroot = e:FindFirstChild("HumanoidRootPart") or e:FindFirstChildOfClass("BasePart")
+					if eh and eroot and eh.Health > 0 then
+						table.insert(targets, {e, eroot})
 					end
 				end
+
 				if HIT_FUNCTION then
-					HIT_FUNCTION(root, args[2])
+					HIT_FUNCTION(root, targets)
 				else
-					RegisterHit:FireServer(unpack(args))
+					RegisterHit:FireServer(root, targets)
 				end
 				RegisterAttack:FireServer(0)
+
+				task.wait()
 			end
 		end
 	end)
